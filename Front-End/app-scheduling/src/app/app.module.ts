77a6f2b56a3903560000b268634dacd6a9a3b1c1
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-07-17 10:54:10 -0300

    adding angular material in button, and updating scss

--- Front-End/app-scheduling/src/app/app.module.ts
+++ Front-End/app-scheduling/src/app/app.module.ts
@@ -1,23 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 import { MatButtonModule } from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent,
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     AppRoutingModule,
-    MatButtonModule,
+    MatButtonModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
