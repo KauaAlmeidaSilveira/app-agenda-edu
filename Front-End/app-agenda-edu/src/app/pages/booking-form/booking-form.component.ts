12a0fa920294f1159a9d398e0339ff8a7664b634
KauaAlmeidaSilveira <kauiis.a.silveira@gmail.com>
2024-07-29 09:30:02 -0300

    updating mat-select input with group options

--- Front-End/app-agenda-edu/src/app/pages/booking-form/booking-form.component.ts
+++ Front-End/app-agenda-edu/src/app/pages/booking-form/booking-form.component.ts
@@ -1,76 +1,104 @@
 import { Component } from '@angular/core';
 
+interface Turma {
+	value: string,
+	viewValue: string
+}
+
+interface TurmaGroup {
+	name: string,
+	turma: Turma[]
+}
+
 @Component({
-  selector: 'app-booking-form',
-  templateUrl: './booking-form.component.html',
-  styleUrl: './booking-form.component.scss'
+	selector: 'app-booking-form',
+	templateUrl: './booking-form.component.html',
+	styleUrl: './booking-form.component.scss'
 })
 export class BookingFormComponent {
 
-  booking = {
-    local: null,
-    date: null,
-    horarioEntrada: null,
-    horarioSaida: null,
-    turma: null,
-  }
+	booking = {
+		local: null,
+		date: null,
+		horarioEntrada: null,
+		horarioSaida: null,
+		turma: null,
+	}
 
-  locais = [
-    { value: 'Sala de informática', viewValue: 'Sala de informática' },
-    { value: 'Sala de ciências', viewValue: 'Sala de ciências' },
-    { value: 'Sala de artes', viewValue: 'Sala de artes' },
-    { value: 'Sala de psicomotricidade', viewValue: 'Sala de psicomotricidade' }
-  ];
+	locais = [
+		{ value: 'Sala de informática', viewValue: 'Sala de informática' },
+		{ value: 'Sala de ciências', viewValue: 'Sala de ciências' },
+		{ value: 'Sala de artes', viewValue: 'Sala de artes' },
+		{ value: 'Sala de psicomotricidade', viewValue: 'Sala de psicomotricidade' }
+	];
 
-  horarioEntrada = [
-    { value: '07:00', viewValue: '07:00' },
-    { value: '08:00', viewValue: '08:00' },
-    { value: '09:00', viewValue: '09:00' },
-    { value: '10:00', viewValue: '10:00' },
-    { value: '11:00', viewValue: '11:00' },
-    { value: '12:00', viewValue: '12:00' },
-    { value: '13:00', viewValue: '13:00' },
-    { value: '14:00', viewValue: '14:00' },
-    { value: '15:00', viewValue: '15:00' },
-    { value: '16:00', viewValue: '16:00' },
-    { value: '17:00', viewValue: '17:00' },
-    { value: '18:00', viewValue: '18:00' },
-    { value: '19:00', viewValue: '19:00' },
-  ];
+	horarioEntrada = [
+		{ value: '07:00', viewValue: '07:00' },
+		{ value: '08:00', viewValue: '08:00' },
+		{ value: '09:00', viewValue: '09:00' },
+		{ value: '10:00', viewValue: '10:00' },
+		{ value: '11:00', viewValue: '11:00' },
+		{ value: '12:00', viewValue: '12:00' },
+		{ value: '13:00', viewValue: '13:00' },
+		{ value: '14:00', viewValue: '14:00' },
+		{ value: '15:00', viewValue: '15:00' },
+		{ value: '16:00', viewValue: '16:00' },
+		{ value: '17:00', viewValue: '17:00' },
+		{ value: '18:00', viewValue: '18:00' },
+		{ value: '19:00', viewValue: '19:00' },
+	];
 
-  horarioSaida = [
-    { value: '07:00', viewValue: '07:00' },
-    { value: '08:00', viewValue: '08:00' },
-    { value: '09:00', viewValue: '09:00' },
-    { value: '10:00', viewValue: '10:00' },
-    { value: '11:00', viewValue: '11:00' },
-    { value: '12:00', viewValue: '12:00' },
-    { value: '13:00', viewValue: '13:00' },
-    { value: '14:00', viewValue: '14:00' },
-    { value: '15:00', viewValue: '15:00' },
-    { value: '16:00', viewValue: '16:00' },
-    { value: '17:00', viewValue: '17:00' },
-    { value: '18:00', viewValue: '18:00' },
-    { value: '19:00', viewValue: '19:00' },
-  ];
+	horarioSaida = [
+		{ value: '07:00', viewValue: '07:00' },
+		{ value: '08:00', viewValue: '08:00' },
+		{ value: '09:00', viewValue: '09:00' },
+		{ value: '10:00', viewValue: '10:00' },
+		{ value: '11:00', viewValue: '11:00' },
+		{ value: '12:00', viewValue: '12:00' },
+		{ value: '13:00', viewValue: '13:00' },
+		{ value: '14:00', viewValue: '14:00' },
+		{ value: '15:00', viewValue: '15:00' },
+		{ value: '16:00', viewValue: '16:00' },
+		{ value: '17:00', viewValue: '17:00' },
+		{ value: '18:00', viewValue: '18:00' },
+		{ value: '19:00', viewValue: '19:00' },
+	];
 
-  turmas = [
-    { value: '1º Ano Fundamental', viewValue: '1º Ano Fundamental' },
-    { value: '2º Ano Fundamental', viewValue: '2º Ano Fundamental' },
-    { value: '3º Ano Fundamental', viewValue: '3º Ano Fundamental' },
-    { value: '4º Ano Fundamental', viewValue: '4º Ano Fundamental' },
-    { value: '5º Ano Fundamental', viewValue: '5º Ano Fundamental' },
-    { value: '6º Ano Fundamental', viewValue: '6º Ano Fundamental' },
-    { value: '7º Ano Fundamental', viewValue: '7º Ano Fundamental' },
-    { value: '8º Ano Fundamental', viewValue: '8º Ano Fundamental' },
-    { value: '9º Ano Fundamental', viewValue: '9º Ano Fundamental' },
-    { value: '1º Ano Médio', viewValue: '1º Ano Médio' },
-    { value: '2º Ano Médio', viewValue: '2º Ano Médio' },
-    { value: '3º Ano Médio', viewValue: '3º Ano Médio' }
-  ];
+	turmaGroups: TurmaGroup[] = [
+		{
+			name: 'Ensino Infantil',
+			turma: [
+				{ value: 'Maternal', viewValue: 'Maternal' },
+				{ value: 'Jardim', viewValue: 'Jardim' },
+				{ value: 'Pre', viewValue: 'Pré' }
+			]
+		},
+		{
+			name: 'Ensino Fundamental',
+			turma: [
+				{ value: '1º Ano', viewValue: '1º Ano' },
+				{ value: '2º Ano', viewValue: '2º Ano' },
+				{ value: '3º Ano', viewValue: '3º Ano' },
+				{ value: '4º Ano', viewValue: '4º Ano' },
+				{ value: '5º Ano', viewValue: '5º Ano' },
+				{ value: '6º Ano', viewValue: '6º Ano' },
+				{ value: '7º Ano', viewValue: '7º Ano' },
+				{ value: '8º Ano', viewValue: '8º Ano' },
+				{ value: '9º Ano', viewValue: '9º Ano' },
+			]
+		},
+		{
+			name: 'Ensino Médio',
+			turma: [
+				{ value: '1º Serie', viewValue: '1º Serie' },
+				{ value: '2º Serie', viewValue: '2º Serie' },
+				{ value: '3º Serie', viewValue: '3º Serie' }
+			]
+		}
+	]
 
-  showInfoBooking() {
-    console.log(this.booking)
-  }
+	showInfoBooking() {
+		console.log(this.booking)
+	}
 
 }
